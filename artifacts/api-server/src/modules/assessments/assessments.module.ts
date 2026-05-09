ded546be5d6bc7a582519e9851c3cd57b8bcea84
SpartanIron <161258894+SpartanIron@users.noreply.github.com>
2026-05-09 11:06:43 -0400

    Sprint 4: Register ReportGeneratorService in AssessmentsModule

--- artifacts/api-server/src/modules/assessments/assessments.module.ts
+++ artifacts/api-server/src/modules/assessments/assessments.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AssessmentsController } from "./assessments.controller";
 import { AssessmentsService } from "./assessments.service";
+import { ReportGeneratorService } from "./report-generator.service";
 
 @Module({
   controllers: [AssessmentsController],
-  providers: [AssessmentsService],
-  exports: [AssessmentsService],
+  providers: [AssessmentsService, ReportGeneratorService],
+  exports: [AssessmentsService, ReportGeneratorService],
 })
 export class AssessmentsModule {}
