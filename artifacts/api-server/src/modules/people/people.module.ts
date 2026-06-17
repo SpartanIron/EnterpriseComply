2e79210da69933ba63bc4dc7135fecc445489137
SpartanIron <161258894+SpartanIron@users.noreply.github.com>
2026-06-17 14:42:49 -0400

    fix(people-module): import NotificationsModule for PeopleService DI

--- artifacts/api-server/src/modules/people/people.module.ts
+++ artifacts/api-server/src/modules/people/people.module.ts
@@ -1,6 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PeopleController } from "./people.controller";
 import { PeopleService } from "./people.service";
+import { NotificationsModule } from "../notifications/notifications.module";
 
-@Module({ controllers: [PeopleController], providers: [PeopleService] })
+@Module({
+  imports: [NotificationsModule],
+  controllers: [PeopleController],
+  providers: [PeopleService],
+})
 export class PeopleModule {}
