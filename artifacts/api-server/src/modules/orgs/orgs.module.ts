858941c439129f9445dc6a5ee263bb1ce92e19e6
SpartanIron <161258894+SpartanIron@users.noreply.github.com>
2026-08-20 18:01:23 -0400

    feat(invites): register InvitesController and InvitesService

--- artifacts/api-server/src/modules/orgs/orgs.module.ts
+++ artifacts/api-server/src/modules/orgs/orgs.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
+import { InvitesController } from "./invites.controller";
+import { InvitesService } from "./invites.service";
 import { OrgsController } from "./orgs.controller";
 import { OrgsService } from "./orgs.service";
 
-@Module({ controllers: [OrgsController], providers: [OrgsService] })
+@Module({ controllers: [OrgsController, InvitesController], providers: [OrgsService, InvitesService] })
 export class OrgsModule {}
